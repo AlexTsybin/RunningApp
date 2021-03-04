6f641ef907745ceaca25671c4f1bf2d9c366df71
alextsybin <alexander.tsyin@gmail.com>
2021-03-04 14:37:47 +0300

    Implementing the Stop Watch

--- app/src/main/java/com/alextsy/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/alextsy/runningapp/ui/fragments/TrackingFragment.kt
@@ -12,6 +12,7 @@ import com.alextsy.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.alextsy.runningapp.other.Constants.MAP_ZOOM
 import com.alextsy.runningapp.other.Constants.POLYLINE_COLOR
 import com.alextsy.runningapp.other.Constants.POLYLINE_WIDTH
+import com.alextsy.runningapp.other.TrackingUtility
 import com.alextsy.runningapp.services.Polyline
 import com.alextsy.runningapp.services.TrackingService
 import com.alextsy.runningapp.ui.viewmodels.MainViewModel
@@ -30,6 +31,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var pathPoints = mutableListOf<Polyline>()
 
     private var map: GoogleMap? = null
+
+    private var curTimeInMillis = 0L
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -55,6 +58,12 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             pathPoints = it
             addLatestPolyline()
             moveCameraToUser()
+        })
+
+        TrackingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
+            curTimeInMillis = it
+            val formattedTime = TrackingUtility.getFormattedStopWatchTime(curTimeInMillis, true)
+            tvTimer.text = formattedTime
         })
     }
 
