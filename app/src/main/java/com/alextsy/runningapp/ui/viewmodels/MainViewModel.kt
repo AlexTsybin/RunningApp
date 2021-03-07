ebebe45c00c5e066b5fea5e61ab6ce191c7a1d7c
alextsybin <alexander.tsyin@gmail.com>
2021-03-07 23:30:56 +0300

    Saving a Run in the Database

--- app/src/main/java/com/alextsy/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/alextsy/runningapp/ui/viewmodels/MainViewModel.kt
@@ -2,9 +2,16 @@ package com.alextsy.runningapp.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alextsy.runningapp.db.Run
 import com.alextsy.runningapp.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
